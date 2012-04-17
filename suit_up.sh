f0beefb6f1f250994d2409bf2be3977c908d8844
Luke Hatcher <lukeman@gmail.com>
2012-04-17 13:49:20 -0400

    no clone fallback

--- suit_up.sh
+++ suit_up.sh
@@ -4,9 +4,7 @@
 # Ben Hoskings' `clone_and_link.sh` located at:
 # <https://github.com/benhoskings/dot-files/blob/master/clone_and_link.sh>
 
-cd &&
-[ -d '.files' ] || git clone git://github.com/bryanveloso/dotfiles.git .files &&
-ls -1d .files/files/* .files/files/.* | while read f; do
+cd && [ -d '.files' ] && ls -1d .files/files/* .files/files/.* | while read f; do
     [ "$f" == '.files/files/.' ] ||
     [ "$f" == '.files/files/..' ] ||
     [ "$f" == '.files/files/.git' ] ||
