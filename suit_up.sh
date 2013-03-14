8fb9de0380341379d2b4bff8d1a66b1ac4cae50b
Luke Hatcher <lukeman@gmail.com>
2013-03-13 22:03:46 -0400

    remove comment

--- suit_up.sh
+++ suit_up.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# This file is entirely inspired by "How I Met Your Mother" and
-# Ben Hoskings' `clone_and_link.sh` located at:
-# <https://github.com/benhoskings/dot-files/blob/master/clone_and_link.sh>
-
 cd && [ -d '.files' ] && ls -1d .files/files/* .files/files/.* | while read f; do
     [ "$f" == '.files/files/.' ] ||
     [ "$f" == '.files/files/..' ] ||
